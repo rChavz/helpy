1af6fff4f63b074498f2de17eb147463416ab15b
Raul Chavez <rh.chavz@gmail.com>
2015-11-09 17:50:12 -0600

    Update nginx.py

--- install/nginx.py
+++ install/nginx.py
@@ -4,3 +4,19 @@ name=nginx repo
 baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
 gpgcheck=0
 enabled=1"""
+
+def file_exists(filepath):
+  #{
+  import os.path
+  return os.path.exists(filepath)
+  #}
+  
+def file_create (filepath, connten)
+  #{
+  print(filepath, connten)
+  #}
+  
+if(file_exists("/etc/yum.repos.d/nginx.repo")):
+  #{
+  file_create("/etc/yum.repos.d/nginx.repo", nginxrepo)
+  #}
